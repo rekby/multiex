78ec8694c80895b223953438703de5e691121d33
rekby <timofey@koolin.ru>
2015-04-24 23:44:13 +0300

    refactor documentation, russian translation
    переработаана документация, добавлен русский перевод
    
    small refactoring
    мелкие правки

--- multiex.go
+++ multiex.go
@@ -8,51 +8,72 @@ import (
 )
 
 // Struct for describe included commands
-// ATTENTION: don't use short initialization form of structure - it can have additional fields in the feauture.
+// ATTENTION: don't use short initialization form of structure (without field names) - it can have additional fields in
+// the feauture.
+
+// Структура для описания подключаемой команды
+// ВНИМАНИЕ: не используйте короткую форму инициализации структуры (без указания имен полей) - структура может
+// дополняться в будущем и использование короткой формы сломает код при обновлении библиотеки
 type ExecutorDescribe struct {
-	Function  func() // Pointer for function
-	Name      string // Name os command what was called
-	NoInstall bool   // Don't create symlink when multiex install called
+	Function  func() // Pointer for function. Указатель на функцию.
+	Name      string // Name os command what was called. Имя команды, по которому функция будет вызываться.
+	NoInstall bool   // Don't create symlink when multiex install called. Не создавать ссылку при вызове команды установки.
 }
 
+// Task for function registration
+// Описание задачи для регистрации функции
 type registerQueueTask struct {
 	module ExecutorDescribe
 	err    chan error
 }
 
+// Golang hasn't explicit gurantee about sequense exec of init function. Create chan for gurantee of registration.
+// Golang не гарантирует строго последовательного вызова функции init. Создаем канал для гарантии поочередной регистрации функций.
 var registerQueue = make(chan registerQueueTask)
+
+// Map of funcctions: name - describe
+// Карта функций: имя - описание
 var executors = make(map[string]ExecutorDescribe)
 
+// Call executor-function by description
+// Вызвать функцию-исполнитель по описанию
 func callWorker(module ExecutorDescribe) {
 	f := reflect.ValueOf(module.Function)
 	f.Call([]reflect.Value{})
 }
 
-func registerWorker() {
+// Start goroutines for register workers.
+// Запускает горутины для регистрации обработчиков
+func startRegisterWorker() {
 	var registerTask registerQueueTask
 
-	// This is lifetime goroutine
-	defer func() {
-		if registerTask.err != nil {
-			err := recover()
-			registerTask.err <- err.(error)
-		}
-		registerWorker() // restore infinite loop
-	}()
-
 	// infinite loop for add modules from queue
-	for {
-		registerTask = <-registerQueue
-		if _, has := executors[registerTask.module.Name]; has {
-			registerTask.err <- errors.New("Module has registered already: '" + registerTask.module.Name + "'")
-		} else {
-			executors[registerTask.module.Name] = registerTask.module
-			registerTask.err <- nil
+	// бесконечный цикл для добавления функций из очереди
+	go func() {
+		// This defer function ensure infinite goroutine for sequence registration of workers
+		// Эта отложенная функция гарантирует непрерывное выполнение горутины для последовательной регистрации обработчиков
+
+		defer func() {
+			if err := recover(); err != nil && registerTask.err != nil {
+				registerTask.err <- err.(error)
+			}
+			startRegisterWorker() // restore infinite loop. Восстановить цикл обработки
+		}()
+
+		for {
+			registerTask = <-registerQueue
+			if _, has := executors[registerTask.module.Name]; has {
+				registerTask.err <- errors.New("Module has registered already (модуль уже зарегистрирован): '" + registerTask.module.Name + "'")
+			} else {
+				executors[registerTask.module.Name] = registerTask.module
+				registerTask.err <- nil
+			}
 		}
-	}
+	}()
 }
 
 // Register function as module worker
+// Зарегистрировать функцию как обработчик
 func Register(module ExecutorDescribe) error {
 	if module.Function == nil {
 		return errors.New("Function can't be nil")
@@ -66,6 +87,7 @@ func Register(module ExecutorDescribe) error {
 }
 
 // Function to call from executable module for start process dispatch call
+// Функция для вызова из выполняемого файла для распознавания и вызова итогового обработчика
 func Main() {
 	var commandName string
 
@@ -99,5 +121,5 @@ func Main() {
 }
 
 func init() {
-	go registerWorker()
+	startRegisterWorker()
 }
--- utils.go
+++ utils.go
@@ -11,6 +11,12 @@ func init() {
 }
 
 func MultiExUtilsMain() {
+	if len(os.Args) > 1 && os.Args[1] == "multiex" {
+		oldName := os.Args[0]
+		os.Args = os.Args[1:]
+		os.Args[0] = oldName
+	}
+
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
 		case "ls":
@@ -39,21 +45,21 @@ func createSymlinks() {
 	dirname := filepath.Dir(binary_path)
 	for _, module := range executors {
 		if module.NoInstall {
-			fmt.Printf("Ignore install by module definition: '%s'\n", module.Name)
+			fmt.Printf("Skip create link by func definition (создание ссылки пропущено согласно настроек модуля): '%s'\n", module.Name)
 			continue
 		}
 		link_path := filepath.Join(dirname, module.Name)
 		err = os.Symlink(basename, link_path)
 		if os.IsExist(err) {
-			fmt.Printf("File exists: %s\n", link_path)
+			fmt.Printf("File exists (файл существует): %s\n", link_path)
 		} else if err != nil {
-			fmt.Printf("Error: '%s' while try to create link '%s'\n", err, link_path)
+			fmt.Printf("Error (ошибка): '%s' while try to create link (при попытке создания ссылки) '%s'\n", err, link_path)
 		}
 	}
 }
 
 func printModules() {
-	fmt.Println("List of commands:")
+	fmt.Println("List of commands (список команд):")
 	for key, _ := range executors {
 		fmt.Println(key)
 	}
@@ -70,13 +76,42 @@ Usages:
     When multiex used - one binary executable file can contain many independent utilities with own rules of work.
     Multiex detect right utility by basename of executable command - as busybox.
 
-    Where multiex may be name of file, symlink or hardlink with filename equals to command name. It is usual case and
-    you can omit multiex-command parameter.
+    Where multiex may be name of file, symlink or hardlink with filename equals to command name.
 
     If binary haven't command with same name as call - first argument try usage as command name
 
     Exec
-    multiex multiex install
+    ./multiex multiex install
     for create simlinks for all internal commands
+
+    where ./multiex - any name of binary/link doesn't equal of any registered functions or "multiex".
+    multiex - name of included worker.
+
+    Other commands for multiex module: ls, help, usage
+
+Использование:
+	multies [команда] аргументы
+
+	Multiex позволяет включить множество независимых команд в один исполняемый файл - для сокращения занимаемого места
+	большим количеством мелких утилит, разделяющих одно и то же окружение golang.
+
+	Когда используется multiex - один исполняемый файл включает в себя независимые команды с собственными правилами работы.
+	Правильная команда для выполнения определяется по имени файла, с которым multiex был вызван - по аналогии с busybox.
+
+	Это имя может быть именем бинарного файла, жесткой или символьной ссылкой, имя которых совпадет с зарегистрированным
+	именем функции.
+
+	Если имя выполняемого файла не совпадает ни с одной из зарегистрированных функций - именем функции считается значение
+	первого аргумента.
+	Если и по этому значению обработчик не найден - выводится справка и список зарегистрированных команд.
+
+	Для создания символьных ссылок в той же папке, где лежит выполняемый файл можно выполнить команду
+	./multiex multiex install
+
+	Где ./multiex - любое имя бинарника/ссылки, не совпадающее с зарегистрированными функциями или "multiex".
+	multiex - имя встроенного обработчика
+	install - команда внутреннему обработчику для создания ссылок
+
+	Другие команды встроенного модуля: ls, help, usage
 	`)
 }
